1a7992c101fecf988eec1d10497bde8874a38990
SmashMaker <88116823+SmashMaker@users.noreply.github.com>
2023-12-04 14:29:01 +0100

    Create CreateDB.sql
    Create querry to create tables in DB

---
+++ SQL/CreateBDD.sql
@@ -0,0 +1,54 @@
+-- Table client
+CREATE TABLE Client(
+  
+);
+
+-- Table Paiement
+CREATE TABLE Paiement(
+  
+);
+
+-- Table Stock
+CREATE TABLE Stock(
+  
+);
+
+-- Table Pays
+CREATE TABLE Pays(
+  
+);
+
+-- Table Ville
+CREATE TABLE Ville(
+  
+);
+
+-- Table Adresse
+CREATE TABLE Adresse(
+  
+);
+
+-- Table Personnel
+CREATE TABLE Personnel(
+  
+);
+
+-- Table Commande
+CREATE TABLE Commande(
+  
+);
+
+-- Table Historique_prix
+CREATE TABLE Historique_prix(
+  
+);
+
+-- Table intermédiaire entre Adresse et Client
+CREATE TABLE habiter(
+  
+);
+
+-- Table intermédiaire entre Commande et Stock
+CREATE TABLE appartenir(
+  
+);
