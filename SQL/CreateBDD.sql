cd167a4eb1d8a111fcb3ebe91cf69d6e1f8eb583
Hugo B <115709939+Yug0-o@users.noreply.github.com>
2023-12-05 11:51:00 +0100

    Update CreateBDD.sql
    new DB

--- SQL/CreateBDD.sql
+++ SQL/CreateBDD.sql
@@ -16,7 +16,6 @@ DROP TABLE IF EXISTS Client;
 
 
 -- #### Création des tables ####
--- Table client
 CREATE TABLE Client(
    id_client INT,
    nom_client VARCHAR(50),
@@ -25,19 +24,7 @@ CREATE TABLE Client(
    PRIMARY KEY(id_client)
 );
 
--- Table Paiement
-CREATE TABLE Paiement(
-   id_paiement INT,
-   date_paiement DATE,
-   moyen_paiement VARCHAR(50),
-   date_reglement DATE,
-   id_client INT NOT NULL,
-   PRIMARY KEY(id_paiement),
-   FOREIGN KEY(id_client) REFERENCES Client(id_client)
-);
-
--- Table Stock
-CREATE TABLE Stock(
+CREATE TABLE Produits(
    id_produit INT,
    nom_produit VARCHAR(50),
    ref_produit VARCHAR(50),
@@ -48,14 +35,12 @@ CREATE TABLE Stock(
    PRIMARY KEY(id_produit)
 );
 
--- Table Pays
 CREATE TABLE Pays(
    id_pays INT,
    nom_pays VARCHAR(50),
    PRIMARY KEY(id_pays)
 );
 
--- Table Ville
 CREATE TABLE Ville(
    id_ville INT,
    nom_ville VARCHAR(50),
@@ -65,18 +50,14 @@ CREATE TABLE Ville(
    FOREIGN KEY(id_pays) REFERENCES Pays(id_pays)
 );
 
--- Table Adresse
 CREATE TABLE Adresse(
    id_adresse INT,
    nom_adresse VARCHAR(50),
-   livraison BIT,
-   facturation BIT,
    id_ville INT NOT NULL,
    PRIMARY KEY(id_adresse),
    FOREIGN KEY(id_ville) REFERENCES Ville(id_ville)
 );
 
--- Table Personnel
 CREATE TABLE Personnel(
    id_personnel VARCHAR(50),
    nom_personnel VARCHAR(50),
@@ -85,38 +66,46 @@ CREATE TABLE Personnel(
    date_embauche DATE,
    id_adresse INT NOT NULL,
    PRIMARY KEY(id_personnel),
-   UNIQUE(id_adresse),
    FOREIGN KEY(id_adresse) REFERENCES Adresse(id_adresse)
 );
 
--- Table Commande
 CREATE TABLE Commande(
    id_commande VARCHAR(50),
    date_emission DATE,
    date_livraison DATE,
    quantite_article INT,
    total_article INT,
-   total_ht DECIMAL(15,2),
-   total_tva DECIMAL(15,2),
-   total_ttc DECIMAL(15,2),
-   id_paiement INT NOT NULL,
+   total_ht CURRENCY,
+   total_tva CURRENCY,
+   total_ttc CURRENCY,
+   id_adresse INT NOT NULL,
    id_client INT NOT NULL,
    PRIMARY KEY(id_commande),
-   FOREIGN KEY(id_paiement) REFERENCES Paiement(id_paiement),
+   FOREIGN KEY(id_adresse) REFERENCES Adresse(id_adresse),
    FOREIGN KEY(id_client) REFERENCES Client(id_client)
 );
 
--- Table Historique_prix
+CREATE TABLE Paiement(
+   id_paiement INT,
+   date_paiement DATE,
+   moyen_paiement VARCHAR(50),
+   montant_paiement CURRENCY,
+   id_adresse INT NOT NULL,
+   id_commande VARCHAR(50) NOT NULL,
+   PRIMARY KEY(id_paiement),
+   FOREIGN KEY(id_adresse) REFERENCES Adresse(id_adresse),
+   FOREIGN KEY(id_commande) REFERENCES Commande(id_commande)
+);
+
 CREATE TABLE Historique_prix(
    id_historique INT,
-   prix_article DECIMAL(15,2),
+   prix_article CURRENCY,
    date_article DATE,
    id_produit INT NOT NULL,
    PRIMARY KEY(id_historique),
-   FOREIGN KEY(id_produit) REFERENCES Stock(id_produit)
+   FOREIGN KEY(id_produit) REFERENCES Produits(id_produit)
 );
 
--- Table intermédiaire entre Adresse et Client
 CREATE TABLE habiter(
    id_adresse INT,
    id_client INT,
@@ -125,11 +114,10 @@ CREATE TABLE habiter(
    FOREIGN KEY(id_client) REFERENCES Client(id_client)
 );
 
--- Table intermédiaire entre Commande et Stock
 CREATE TABLE appartenir(
    id_commande VARCHAR(50),
    id_produit INT,
    PRIMARY KEY(id_commande, id_produit),
    FOREIGN KEY(id_commande) REFERENCES Commande(id_commande),
-   FOREIGN KEY(id_produit) REFERENCES Stock(id_produit)
+   FOREIGN KEY(id_produit) REFERENCES Produits(id_produit)
 );
