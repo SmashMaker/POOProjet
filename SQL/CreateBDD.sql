5bf457b3522e859c776af95fe2748692e6a4e6e1
Hugo B <115709939+Yug0-o@users.noreply.github.com>
2023-12-07 10:52:28 +0100

    Update CreateBDD.sql
    ajout de l'auto-increment

--- SQL/CreateBDD.sql
+++ SQL/CreateBDD.sql
@@ -15,9 +15,12 @@ DROP TABLE IF EXISTS Ville;
 DROP TABLE IF EXISTS Pays;
 
 
+USE PRojet_POO;
+
+
 -- #### Création des tables ####
 CREATE TABLE Client(
-   id_client INT,
+   id_client INT IDENTITY(1,1),
    nom_client VARCHAR(50),
    prenom_client VARCHAR(50),
    date_naissance DATE,
@@ -25,7 +28,7 @@ CREATE TABLE Client(
 );
 
 CREATE TABLE Produit(
-   id_produit INT,
+   id_produit INT IDENTITY(1,1),
    nom_produit VARCHAR(50),
    ref_produit VARCHAR(50),
    designation VARCHAR(50),
@@ -36,13 +39,13 @@ CREATE TABLE Produit(
 );
 
 CREATE TABLE Pays(
-   id_pays INT,
+   id_pays INT IDENTITY(1,1),
    nom_pays VARCHAR(50),
    PRIMARY KEY(id_pays)
 );
 
 CREATE TABLE Ville(
-   id_ville INT,
+   id_ville INT IDENTITY(1,1),
    nom_ville VARCHAR(50),
    code_postal VARCHAR(10),
    id_pays INT NOT NULL,
@@ -51,7 +54,7 @@ CREATE TABLE Ville(
 );
 
 CREATE TABLE Adresse(
-   id_adresse INT,
+   id_adresse INT IDENTITY(1,1),
    nom_adresse VARCHAR(50),
    id_ville INT NOT NULL,
    PRIMARY KEY(id_adresse),
@@ -59,12 +62,12 @@ CREATE TABLE Adresse(
 );
 
 CREATE TABLE Personnel(
-   id_personnel VARCHAR(50),
+   id_personnel INT IDENTITY(1,1),
    nom_personnel VARCHAR(50),
    prenom_personnel VARCHAR(50),
    superieur_personnel VARCHAR(50),
    date_embauche DATE,
-   id_adresse INT NOT NULL,
+   id_adresse INT,
    PRIMARY KEY(id_personnel),
    FOREIGN KEY(id_adresse) REFERENCES Adresse(id_adresse)
 );
@@ -78,15 +81,15 @@ CREATE TABLE Commande(
    total_ht MONEY,
    total_tva MONEY,
    total_ttc MONEY,
-   id_adresse INT NOT NULL,
-   id_client INT NOT NULL,
+   id_adresse INT,
+   id_client INT,
    PRIMARY KEY(id_commande),
    FOREIGN KEY(id_adresse) REFERENCES Adresse(id_adresse),
    FOREIGN KEY(id_client) REFERENCES Client(id_client)
 );
 
 CREATE TABLE Paiement(
-   id_paiement INT,
+   id_paiement INT IDENTITY(1,1),
    date_paiement DATE,
    moyen_paiement VARCHAR(50),
    montant_paiement MONEY,
@@ -98,7 +101,7 @@ CREATE TABLE Paiement(
 );
 
 CREATE TABLE Historique_prix(
-   id_historique INT,
+   id_historique INT IDENTITY(1,1),
    prix_article MONEY,
    date_article DATE,
    id_produit INT NOT NULL,
